956288c3efc38adcb72feb8ab1672c423cadc93b
ACDesmond <187039836@qq.com>
2019-07-21 17:20:41 +0800

    1

---
+++ src/main/java/com/ninova/mlc/MlcApplication.java
@@ -0,0 +1,13 @@
+package com.ninova.mlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ninova/mlc/MlcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ninova.mlc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MlcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
