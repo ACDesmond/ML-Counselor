4b0f6e62bb993aa6464101080dac508de966752a
CheneyZZZ <154593855@qq.com>
2019-08-21 16:27:51 +0800

    trace 04 基金API

--- src/main/java/com/ninova/mlc/blImpl/trace/TraceServiceImpl.java
+++ src/main/java/com/ninova/mlc/blImpl/trace/TraceServiceImpl.java
@@ -52,6 +52,55 @@ public class TraceServiceImpl {
         return null;
     }
 
+    //请求基金数据
+    public static JSONObject getRequest2(String code) {
+        //接口地址
+        String requestUrl = "http://fundgz.1234567.com.cn/js/"+code+".js?rt=1463558676006";
+        Map params = new HashMap();
+        String string = httpRequest(requestUrl,params);
+        string=string.substring(8,string.length()-2);
+        JSONObject object = JSONObject.fromObject(string);
+        System.out.println(object);
+        return object;
+    }
+
+    private static String httpRequest(String requestUrl,Map params) {
+        //buffer用于接受返回的字符
+        StringBuffer buffer = new StringBuffer();
+        try {
+            //建立URL，把请求地址给补全，其中urlencode（）方法用于把params里的参数给取出来
+            URL url = new URL(requestUrl+"?"+urlencode(params));
+            //打开http连接
+            HttpURLConnection httpUrlConn = (HttpURLConnection) url.openConnection();
+            httpUrlConn.setDoInput(true);
+            httpUrlConn.setRequestMethod("GET");
+            httpUrlConn.connect();
+
+            //获得输入
+            InputStream inputStream = httpUrlConn.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            //将bufferReader的值给放到buffer里
+            String str = null;
+            while ((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+            //关闭bufferReader和输入流
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
+            inputStream = null;
+            //断开连接
+            httpUrlConn.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //返回字符串
+        return buffer.toString();
+    }
+
     public static String net(String strUrl, Map params,String method) throws Exception {
         HttpURLConnection conn = null;
         BufferedReader reader = null;
