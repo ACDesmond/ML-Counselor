01e04ee40559bb44e2c21a2260416ed3c61dc401
171250005 <tintin20172017@outlook.com>
2019-08-21 18:20:05 +0800

    trace impl

--- src/main/java/com/ninova/mlc/blImpl/trace/TraceServiceImpl.java
+++ src/main/java/com/ninova/mlc/blImpl/trace/TraceServiceImpl.java
@@ -1,5 +1,9 @@
 package com.ninova.mlc.blImpl.trace;
 
+import com.ninova.mlc.bl.trace.TraceService;
+import com.ninova.mlc.data.PurchaseMapper;
+import com.ninova.mlc.po.PurchaseRecord;
+import com.ninova.mlc.vo.ResponseVO;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +19,122 @@ import java.util.Map;
 
 @Service
 @Repository
-public class TraceServiceImpl {
+public class TraceServiceImpl implements TraceService {
+    @Autowired
+    private PurchaseMapper purchaseMapper;
+
     public static final String DEF_CHATSET = "UTF-8";
     public static final int DEF_CONN_TIMEOUT = 30000;
     public static final int DEF_READ_TIMEOUT = 30000;
     public static String userAgent =  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
+
+
+    @Override
+    public ResponseVO getStockDailyBenefit(int userId, String code){
+        try{
+            float stockDailyBenefit;
+            PurchaseRecord stock=purchaseMapper.selectRecordByUserIdAndCode(userId,code);
+            float num=stock.getNumber();
+            float nowPrice=(float)getRequest(code).get("nowPri");
+            float yestodEndPrice=(float)getRequest(code).get("yestodEndPri");
+            stockDailyBenefit=(nowPrice-yestodEndPrice)*num;
+            return ResponseVO.buildSuccess(stockDailyBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getStockTotalBenefit(int userId,String code){
+        try{
+            double stockTotalBenefit;
+            PurchaseRecord stock=purchaseMapper.selectRecordByUserIdAndCode(userId,code);
+            float num=stock.getNumber();
+            float nowPrice=(float)getRequest(code).get("nowPri");
+            double principle=stock.getPrincipal();
+            stockTotalBenefit=nowPrice*num-principle;
+            return ResponseVO.buildSuccess(stockTotalBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getfundDailyBenefit(int userId,String code){
+        try{
+            double fundDailyBenefit=0.0;
+            return ResponseVO.buildSuccess(fundDailyBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getfundTotalBenefit(int userId,String code){
+        try{
+            double fundTotalBenefit=0.0;
+            return ResponseVO.buildSuccess(fundTotalBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getbondDailyBenefit(int userId,String code){
+        try{
+            double bondDailyBenefit=0.0;
+            return ResponseVO.buildSuccess(bondDailyBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getbondTotalBenefit(int userId,String code){
+        try{
+            double bondTotalBenefit=0.0;
+            return ResponseVO.buildSuccess(bondTotalBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getCFPDailyBenefit(int userId,String code){
+        try{
+            double CFPDailyBenefit=0.0;
+            return ResponseVO.buildSuccess(CFPDailyBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
+    @Override
+    public ResponseVO getCFPTotalBenefit(int userId,String code){
+        try{
+            double CFPTotalBenefit=0.0;
+            return ResponseVO.buildSuccess(CFPTotalBenefit);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return ResponseVO.buildFailure("失败");
+        }
+    }
+
 
     //申请的KEY
     public static final String APPKEY ="a04675114c99983de7e893dbaca4896c";
